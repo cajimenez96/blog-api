1e122600c7eda90e9bc721d74e6b1409fdc94945
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2024-04-22 16:03:29 -0300

    refactor: se agregan atributos al modelo de usuario

--- model/user.js
+++ model/user.js
@@ -1,19 +1,64 @@
 import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+		minLength: 3,
+		maxLength: 50,
+	},
+	name: {
+		type: String,
+		required: true,
+		minLength: 2,
+		maxLength: 50,
+	},
+	lastName: {
+		type: String,
+		required: true,
+		minLength: 2,
+		maxLength: 50,
+	},
+	password: {
+		type: String,
+		required: true,
+		minLength: 4,
+		maxLength: 100,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		maxLength: 100,
+	},
+	phoneNumber: {
+		type: String,
+		maxLength: 30,
+	},
+	address: {
+		type: String,
+		minLength: 10,
+		maxLength: 100,
+	},
+	country: {
+		type: String,
+		required: true,
+		minLength: 4,
+		maxLength: 60,
+	},
+	birthday: {
+		type: Date,
+		required: true,
+	},
+	role: {
+		type: String,
+		enum: {
+			values: ['rol1', 'rol2', 'rol3'], //TODO: definir roles
+			message: '{VALUE} no es un rol valido',
+		},
+		required: true,
+	},
 });
 
 
