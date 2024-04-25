56b73af649bc7685abce3ad3bc94d0d0cbf88ca1
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2024-04-25 19:07:13 -0300

    feature/added id and email regex for validation

--- utils/constants.js
+++ utils/constants.js
@@ -1,1 +1,3 @@
-export const SALT_ROUNDS = 10;
+export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{4,}$/;
+
+export const ID_REGEX = /^[0-9a-fA-F]{24}$/;
